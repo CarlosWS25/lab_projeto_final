0c84cab613b1608686ec26f24c9b113969ccf88d
JotaK14 <jpjoaopedroandrade@gmail.com>
2025-04-04 12:39:11 +0100

    Tela de Loading
    + Criação da tela de loading com a logo da empresa (main.dart);
    + Adição de fontes e imagens (pubspec.yaml);

--- dosewise/lib/main.dart
+++ dosewise/lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
+import"package:flutter/material.dart";
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
@@ -10,11 +10,76 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          title: const Text('Teste'),
-        ),
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+  @override
+  SplashScreenState createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Future.delayed(Duration(seconds: 5), () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => HomeScreen()),
+        );
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor:Color(0xFFA7C4E2),
+      body: Center( 
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/images/logo_dosewise.png",
+              width: 300,
+              height: 300,
+              fit: BoxFit.contain),
+            SizedBox(height: 20),
+            const Text("DoseWise",
+              style: TextStyle(
+                fontFamily: "Fontspring-DEMO-clarikaprogeo-md",  
+                fontWeight: FontWeight.bold,
+                fontSize: 50,
+                color:Color(0xFF1B3568),)
+            ), 
+            const Text("Overdose Control App",
+            style: TextStyle(
+                fontFamily: "Fontspring-DEMO-clarikaprogeo-md",
+                fontSize: 20,
+                color:Color(0xFF1B3568),)
+            )
+          ]
+        )
+      )    
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF1B3568),
+        title: const Text("DoseWise"),
+      ),
+      body: Center(
+        child: const Text("Bem-vindo à Tela Principal!"),
       ),
     );
   }
